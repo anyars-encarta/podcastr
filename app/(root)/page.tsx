e4e4264e87c243c25f519e664e90d68a9e62fbb2
anyars-encarta <anyarsencarta@gmail.com>
2024-06-05 10:31:30 +0000

    Add paraqmeters to PodcastCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+import PodcastCard from '@/components/PodcastCard';
+import { podcastData } from '@/constants';
 import React from 'react';
 
 const Home = () => {
@@ -5,6 +7,15 @@ const Home = () => {
     <div className='mt-9 flex fle-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+        {podcastData.map(({ id, title, description, imgURL }) => (
+          <PodcastCard
+            key={id}
+            imgUrl={imgURL}
+            title={title}
+            description={description}
+            podcastId={id}
+          />
+        ))}
       </section>
     </div>
   )
---
+++ components/PodcastCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PodcastCard = ({ title, description, imgURL, podcastId }: { title: string, description: string, imgURL: string, podcastId: number}) => {
+  return (
+    <div>PodcastCard</div>
+  )
+}
+
+export default PodcastCard
