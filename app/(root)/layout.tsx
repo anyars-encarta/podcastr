e65d234c4896665275a73fd216b955a013e20e96
anyars-encarta <anyarsencarta@gmail.com>
2024-06-05 08:03:08 +0000

    Adding a Mobile Nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
-import LeftSidebar from "@/components/leftSidebar";
+import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,12 +9,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
-            <LeftSidebar />
-            {children}
-            <p className='text-white-1'>RIGHT SIDEBAR</p>
-        </main>
+    <div className="relative flex flex-col">
+      <main className='relative flex bg-black-3'>
+        <LeftSidebar />
+
+        <section className='border-2 border-red-500 flex min-h-screen flex-1 flex-col p-4 sm:p-14'>
+          <div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
+            <div className='flex h-16 items-center justify-between md:hidden'>
+              <Image src='/icons/logo.svg' width={30} height={30} alt="menu icon"/>
+              <MobileNav />
+            </div>
+
+            <div>
+              Toaster (notification popups)
+              {children}
+            </div>
+          </div>
+        </section>
+
+          <RightSidebar />
+      </main>
     </div>
   );
 }
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <div className='text-white-1'>RightSidebar</div>
+  )
+}
+
+export default RightSidebar
