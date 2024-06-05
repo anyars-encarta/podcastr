b5729a1a6ed60229af5751238df5e379c4ee4af1
anyars-encarta <anyarsencarta@gmail.com>
2024-06-05 08:09:40 +0000

    Add a sidebar styling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <main className='relative flex bg-black-3'>
         <LeftSidebar />
 
-        <section className='border-2 border-red-500 flex min-h-screen flex-1 flex-col p-4 sm:p-14'>
+        <section className='flex min-h-screen flex-1 flex-col p-4 sm:p-14'>
           <div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
             <div className='flex h-16 items-center justify-between md:hidden'>
               <Image src='/icons/logo.svg' width={30} height={30} alt="menu icon"/>
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const RightSidebar = () => {
   return (
-    <div className='text-white-1'>RightSidebar</div>
+    <section className='right_sidebar text-white-1'>
+      Right Sidebar
+    </section>
   )
 }
 
