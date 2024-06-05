4a3e5d4601f93d36836c15cd81e34849caa253e1
anyars-encarta <anyarsencarta@gmail.com>
2024-06-05 12:41:16 +0000

    Implement Discover page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,15 +7,17 @@ const Home = () => {
     <div className='mt-9 flex fle-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
-        {podcastData.map(({ id, title, description, imgURL }) => (
-          <PodcastCard
-            key={id}
-            imgURL={imgURL}
-            title={title}
-            description={description}
-            podcastId={id}
-          />
-        ))}
+        <div className='podcast_grid'>
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard
+              key={id}
+              imgURL={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   )
--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -10,7 +10,18 @@ const PodcastCard = (
     return (
         <div className='cursor-pointer '>
             <figure className='flex flex-col gap-2'>
-                <Image src={imgURL} alt={title} width={174} height={174}/>
+                <Image 
+                src={imgURL} 
+                alt={title} 
+                width={174} 
+                height={174}
+                className='aspect-square h-fit w-full rounded-xl 2xl:size-[200px]'
+                 />
+
+                <div className='flex flex-col'>
+                    <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
+                    <h2 className='text-12 truncate font-normal capitalize text-white-4'>{description}</h2>
+                </div>
             </figure>
         </div>
     )
