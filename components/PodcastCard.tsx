89a977cc8cd42b1e5a402baa514016b39a0fa6b4
anyars-encarta <anyarsencarta@gmail.com>
2024-06-05 10:36:28 +0000

    Correct layout

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const PodcastCard = ({ title, description, imgURL, podcastId }: { title: string, description: string, imgURL: string, podcastId: number}) => {
-  return (
-    <div>PodcastCard</div>
-  )
+const PodcastCard = (
+    { title, description, imgURL, podcastId
+
+    }: {
+        title: string, description: string, imgURL: string, podcastId: number
+    }) => {
+    return (
+        <div>PodcastCard</div>
+    )
 }
 
 export default PodcastCard
