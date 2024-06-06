cfb245b5967ddc3b3b04950dd197d86c985ea732
anyars-encarta <anyarsencarta@gmail.com>
2024-06-06 15:53:24 +0000

    Add sign-in and sign-up routes

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -19,7 +19,19 @@ import { ConvexReactClient } from "convex/react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
-  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+  <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} appearance={{
+    layout: {
+      socialButtonsVariant: 'iconButton',
+      logoImageUrl: '/icons/auth-logo.svg'
+    },
+    variables: {
+      colorBackground: '#15171C',
+      colorPrimary: '',
+      colorText: 'white',
+      colorInputBackground: '#1B1F29',
+      colorInputText: 'white',
+    }
+  }}>
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
     </ConvexProviderWithClerk>
