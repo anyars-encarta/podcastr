a8e89b52a1cde031e768f12d26574e7412422170
anyars-encarta <anyarsencarta@gmail.com>
2024-06-07 09:13:37 +0000

    Install Shadcn select component

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -16,6 +16,13 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -68,7 +75,7 @@ const CreatePodcast = () => {
               <Label className='text-16 font-bold text-white-1'>
                   Select AI Voice
               </Label>
-              
+
             </div>
           </div>
 
