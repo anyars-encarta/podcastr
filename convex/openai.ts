d440f1ad5f1f68d9f289b03ae63ecd4a46e9611d
anyars-encarta <anyarsencarta@gmail.com>
2024-06-08 08:46:34 +0000

    Update openai.ts file

--- convex/openai.ts
+++ convex/openai.ts
@@ -1,18 +1,23 @@
 import { action } from "./_generated/server";
 import { v } from "convex/values";
+import OpenAI from "openai";
+import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const generateAudioAction = action({
-  args: { input: v.number(), voice: v.number() },
-  handler: async (_, args) => {
-    const mp3 = await openai.audio.speech.create({
-        model: "tts-1",
-        voice: "alloy",
-        input: "Today is a wonderful day to build something people love!",
-      });
-      console.log(speechFile);
-      const buffer = Buffer.from(await mp3.arrayBuffer());
-      await fs.promises.writeFile(speechFile, buffer);
-    }
-    return "success";
-  },
+    args: { input: v.number(), voice: v.number() },
+    handler: async (_, { voice, input }) => {
+        const mp3 = await openai.audio.speech.create({
+            model: "tts-1",
+            voice: voice as SpeechCreateParams['voice'],
+            input,
+        });
+        const buffer = Buffer.from(await mp3.arrayBuffer());
+        await fs.promises.writeFile(speechFile, buffer);
+
+        return "success";
+    },
 });
