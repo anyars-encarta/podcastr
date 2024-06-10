e3dc6529e78f4758cd09c66973851db682a12ef5
anyars-encarta <anyarsencarta@gmail.com>
2024-06-10 15:51:04 +0000

    Add AI Thumbnail buttons

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import { Button } from './ui/button'
+import { cn } from '@/lib/utils';
 
 const GenerateThumbnail = () => {
+  const [isAiThumbnail, setIsAiThumbnail] = useState(false);
+
   return (
     <>
       <div className='generate_thumbnail'>
         <Button
           type='button'
           variant='plain'
-          className='bg-black-6'
+          onClick={() => setIsAiThumbnail(true)}
+          className={cn('', {
+            'bg-black-6': isAiThumbnail
+          })}
         >
+          Use AI to generate thumbnail
+        </Button>
 
+        <Button
+          type='button'
+          variant='plain'
+          onClick={() => setIsAiThumbnail(false)}
+          className={cn('', {
+            'bg-black-6': !isAiThumbnail
+          })}
+        >
+          Upload custom image
         </Button>
       </div>
     </>
