03e247702a37045cb652587141a8071c35e6a3b7
anyars-encarta <anyarsencarta@gmail.com>
2024-06-12 12:06:06 +0000

    Update content of details page

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -2,10 +2,11 @@
 
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
-import {trendingPodcasts} from '@/constants/index'
+import { trendingPodcasts } from '@/constants/index'
 import { useQuery } from 'convex/react'
 import Image from 'next/image'
 import React from 'react'
+import PodcastDetailPlayer from '@/components/PodcastDetailPlayer'
 
 const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'podcasts'> } }) => {
     const podcast = useQuery(api.podcasts.getPodcastById, { podcastId })
@@ -24,6 +25,24 @@ const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'po
                     <h2 className='text-16 font-bold text-white-1'>{podcast?.views}</h2>
                 </figure>
             </header>
+
+            <PodcastDetailPlayer />
+
+            <p className='text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center'>
+                {podcast?.podcastDescription}
+            </p>
+
+            <div className='flex flex-col gap-8'>
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-18 font-bold text-white-1'>Transcription</h1>
+                    <p className='text-16 font-medium text-white-2'>{podcast?.voicePrompt}</p>
+                </div>
+
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-18 font-bold text-white-1'>Thumbnail Prompt</h1>
+                    <p className='text-16 font-medium text-white-2'>{podcast?.imgPrompt}</p>
+                </div>
+            </div>
         </section>
     )
 }
---
+++ components/PodcastDetailPlayer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PodcastDetailPlayer = () => {
+  return (
+    <div className='text-xl text-white-1'>PodcastDetailPlayer</div>
+  )
+}
+
+export default PodcastDetailPlayer
