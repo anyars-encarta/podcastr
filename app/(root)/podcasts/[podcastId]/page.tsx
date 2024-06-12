010b697b895a3e1bb6d2a194ec61d2a7cf2f1ed9
anyars-encarta <anyarsencarta@gmail.com>
2024-06-12 14:51:47 +0000

    Add Loader Spinner

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -7,9 +7,14 @@ import { useQuery } from 'convex/react'
 import Image from 'next/image'
 import React from 'react'
 import PodcastDetailPlayer from '@/components/PodcastDetailPlayer'
+import LoaderSpinner from '@/components/LoaderSpinner'
 
 const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'podcasts'> } }) => {
     const podcast = useQuery(api.podcasts.getPodcastById, { podcastId })
+
+    const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, { podcastId })
+
+    if (!similarPodcasts || !podcast) return <LoaderSpinner />
 
     return (
         <section className='flex w-full flex-col'>
@@ -43,6 +48,12 @@ const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'po
                     <p className='text-16 font-medium text-white-2'>{podcast?.imgPrompt}</p>
                 </div>
             </div>
+
+            <section className='mt-8 flex flex-col gap-5'>
+                <h1 className='text-20 font-bold text-white-1'>Similar Podcasts</h1>
+
+
+            </section>
         </section>
     )
 }
