b606e20c8500010aaeff223c955d17f987af7531
anyars-encarta <anyarsencarta@gmail.com>
2024-06-14 11:57:28 +0000

    Add user profile info

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const RightSidebar = () => {
+  const { user } = useUser();
+
   return (
     <section className='right_sidebar text-white-1'>
-      Right Sidebar
+      <SignedIn>
+        <Link href={`/profile/${user?.id}`} className='flex gap-3 pb-12'>
+          <UserButton />
+
+          <div className='flex w-full items-center justify-between'>
+            <h1 className='text-16 truncate font-semibold text-white-1'>{user?.firstName} {user?.lastName}</h1>
+            <Image src='/icons/right-arrow.svg' width={24} height={24} alt='arrow'/>
+          </div>
+        </Link>
+      </SignedIn>
     </section>
   )
 }
