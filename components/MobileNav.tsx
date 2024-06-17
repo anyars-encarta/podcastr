03591bb1c85952640ae5ebfd2b7b213a8c2eb4e6
anyars-encarta <anyarsencarta@gmail.com>
2024-06-17 12:45:42 +0000

    Install Sheet from Shadcn

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
+    <div>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </div>
   )
 }
 
