8b4b51fa9e29962eff60ea44af4c558240bf4cad
anyars-encarta <anyarsencarta@gmail.com>
2024-06-17 15:51:54 +0000

    Complete podcaster

--- components/leftSidebar.tsx
+++ components/leftSidebar.tsx
@@ -8,14 +8,16 @@ import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import { Button } from './ui/button';
 import { SignedIn, SignedOut, useClerk } from '@clerk/nextjs';
+import { useAudio } from '@/providers/AudioProvider';
 
 const LeftSidebar = () => {
     const pathname = usePathname();
     const router = useRouter();
     const { signOut } = useClerk();
+    const { audio } = useAudio();
 
     return (
-        <section className='left_sidebar'>
+        <section className={cn('left_sidebar h-[calc(100vh-5px)]', {'h-[calc(100vh-140px)]': audio?.audioURL})}>
             <nav className='flex flex-col gap-6'>
                 <Link href='/' className='flex cursor-pointer items-center gap-1 pb-10 max-lg: justify-center'>
                     <Image src='/icons/logo.svg' alt='logo' width={23} height={27} />
