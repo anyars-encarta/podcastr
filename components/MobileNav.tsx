758a80fa247f6a15139a090d7610febf3a481577
anyars-encarta <anyarsencarta@gmail.com>
2024-06-17 13:18:30 +0000

    Add a mobile menu

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -37,12 +37,14 @@ const MobileNav = () => {
                 {sidebarLinks.map((item) => {
                   const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
 
-                  return <Link href={item.route} key={item.label} className={cn('flex gap-3 items-center py-4 max-lg:px-4 justify-start', {
-                    'bg-nav-focus border-r-4 border-orange-1': isActive
-                  })}>
-                    <Image src={item.imgURL} alt={item.label} width={24} height={24} />
-                    <p>{item.label}</p>
-                  </Link>
+                  return <SheetClose asChild key={item.route}>
+                    <Link href={item.route} className={cn('flex gap-3 items-center py-4 max-lg:px-4 justify-start', {
+                      'bg-nav-focus border-r-4 border-orange-1': isActive
+                    })}>
+                      <Image src={item.imgURL} alt={item.label} width={24} height={24} />
+                      <p>{item.label}</p>
+                    </Link>
+                  </SheetClose>
                 })}
               </nav>
             </SheetClose>
