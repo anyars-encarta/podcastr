6e81f43b5513e2fe0238df5dd33c7c4d97bfff64
anyars-encarta <anyarsencarta@gmail.com>
2024-06-17 11:08:36 +0000

    Add podcast card to discover page

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,11 +1,44 @@
+'use client'
+
+import EmptyState from '@/components/EmptyState'
+import LoaderSpinner from '@/components/LoaderSpinner'
+import PodcastCard from '@/components/PodcastCard'
+import { api } from '@/convex/_generated/api'
+import { useQuery } from 'convex/react'
 import React from 'react'
 
 const Discover = () => {
+  const podcastsData = useQuery(api.podcasts.getPodcastBySearch, { search: '' });
+
   return (
-    <div>
-      <h1 className='text-20 font-bold text-white-1'>
-        Discover
-      </h1>
+    <div className='flex flex-col gap-9'>
+      Searchbar...
+
+      <div className='flex flex-col gap-9'>
+        <h1 className='text-20 font-bold text-white-1'>Discover</h1>
+
+        {podcastsData ? (
+          <>
+            {podcastsData.length > 0 ? (
+              <div className='podcast_grid'>
+                {podcastsData?.map(({ _id, imgURL, podcastTitle, podcastDescription}) => (
+                  <PodcastCard
+                    key={_id}
+                    imgURL={imgURL!}
+                    title={podcastTitle}
+                    description={podcastDescription}
+                    podcastId={_id}
+                  />
+                ))}
+              </div>
+            ) : (
+              <EmptyState title='No results found' />
+            )}
+          </>
+        ) : (
+          <LoaderSpinner />
+        )}
+      </div>
     </div>
   )
 }
